fbaf32127acfeeb053fce4e7f93355446d40b772
Tanvir Ahmad <tnvir9@gmail.com>
2024-04-10 22:18:48 +0600

    Custom Tailwind class and font setup

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,11 @@
+import Header from "../Shared/Header/Header";
+import Navbar from "../Shared/Navbar/Navbar";
 
 const Home = () => {
     return (
         <div>
+            <Header></Header>
+            <Navbar></Navbar>
             <h2 className='text-3xl font-bold'>This is home</h2>
         </div>
     );
