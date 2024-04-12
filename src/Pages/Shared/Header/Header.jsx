f07d2b4e54116154b9694662f62c1bab336c3d34
Tanvir Ahmad <tnvir9@gmail.com>
2024-04-12 17:30:05 +0600

    Updated Header section

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -3,8 +3,8 @@ import moment from 'moment';
 
 const Header = () => {
     return (
-        <div className="text-center">
-            <img src={logo} alt="" />
+        <div className="flex justify-center items-center flex-col">
+            <img className="" src={logo} alt="" />
             <p>Journalism Without Fear or Favour</p>
             <p className="text-2xl">{moment().format("dddd, MMMM D, YYYY")}</p>
         </div>
