dc06eb9da3b6520426498022d07f5b8ab8cf372e
Tanvir Ahmad <tnvir9@gmail.com>
2024-04-13 10:48:38 +0600

    Register page with form data to collect data

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,16 +1,11 @@
 import { Link } from "react-router-dom";
 import Navbar from "../Shared/Navbar/Navbar";
 
-const Login = () => {
-    const handleLogin = e => {
+const Register = () => {
+    const handleRegister = e => {
         e.preventDefault();
-        // const email = e.target.email.value;
-        // const password = e.target.password.value;
-        // console.log(email,password);
-        // !>>>>>>>>>> Alternative <<<<<<<<<
-        // console.log(e.currentTarget);
         const form = new FormData(e.currentTarget);
-        console.log(form.get('email'), form.get('password'));
+        console.log(form.get('name'),form.get('photoUrl'),form.get('email'),form.get('password'));
     }
   return (
     <div>
@@ -19,10 +14,34 @@ const Login = () => {
         <div className="hero min-h-screen bg-base-200">
           <div className="hero-content flex-col">
             <div className="text-center">
-              <h1 className="text-5xl font-bold">Login now!</h1>
+              <h1 className="text-5xl font-bold">Register now!</h1>
             </div>
             <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-              <form onSubmit={handleLogin} className="card-body">
+              <form onSubmit={handleRegister} className="card-body">
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Name</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="name"
+                    className="input input-bordered"
+                    name="name"
+                    required
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Photo URL</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="photo url"
+                    className="input input-bordered"
+                    name="photoUrl"
+                    required
+                  />
+                </div>
                 <div className="form-control">
                   <label className="label">
                     <span className="label-text">Email</span>
@@ -53,10 +72,15 @@ const Login = () => {
                   </label>
                 </div>
                 <div className="form-control mt-6">
-                  <button className="btn btn-primary">Login</button>
+                  <button className="btn btn-primary">Register</button>
                 </div>
               </form>
-              <p className="p-5">Do not have an account? Please <Link className="text-red-500" to={'/register'}>Register</Link></p>
+              <p className="p-5">
+                Already have an account? Please{" "}
+                <Link className="text-red-500" to={"/login"}>
+                  Login
+                </Link>
+              </p>
             </div>
           </div>
         </div>
@@ -65,4 +89,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Register;
